3ad26b2bdb59b8a066c1f94c534f7359615760c3
Dannylo Johnathan <dannylojohnathan@gmail.com>
2020-04-06 11:26:50 -0300

    primeiro commit, estrutura geral da API

---
+++ src/main/java/br/edu/ifrn/laj/pdcorp/apisea/ApiSeaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.laj.pdcorp.apisea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/laj/pdcorp/apisea/ApiSeaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.laj.pdcorp.apisea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
