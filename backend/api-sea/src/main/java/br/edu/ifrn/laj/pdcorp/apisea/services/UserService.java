c88757931fa659bb0ddc6960e7d007d86b85bbcb
Dannylo Johnathan <dannylojohnathan@gmail.com>
2020-05-20 15:01:00 -0300

    Correção da regra de negócio no save do usuário

--- backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/services/UserService.java
+++ backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/services/UserService.java
@@ -2,7 +2,10 @@ package br.edu.ifrn.laj.pdcorp.apisea.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
+import br.edu.ifrn.laj.pdcorp.apisea.enums.ExceptionMessages;
+import br.edu.ifrn.laj.pdcorp.apisea.exceptions.ApiUserException;
 import br.edu.ifrn.laj.pdcorp.apisea.models.User;
 import br.edu.ifrn.laj.pdcorp.apisea.repository.UserRepository;
 
@@ -12,8 +15,11 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	public User save(User user) {
-		return repository.save(user);
+	public User save(User user) throws ApiUserException {
+		if(!ObjectUtils.isEmpty(repository.findByEmail(user.getEmail()))) {
+			throw new ApiUserException(ExceptionMessages.EMAIL_EXISTS_DB);
+		}
+		return repository.save(user); 
 	}
 
 }
