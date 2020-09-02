96873ff5b8720a93488818b90880354df1d527eb
Fernando Soares <fernandolinharessoares@gmail.com>
2020-09-01 21:03:18 -0300

    Tornando consultas do SubscriptionRepository transacionais

--- backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/repositories/SubscriptionRepository.java
+++ backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/repositories/SubscriptionRepository.java
@@ -2,6 +2,8 @@ package br.edu.ifrn.laj.pdcorp.apisea.repositories;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.edu.ifrn.laj.pdcorp.apisea.models.Event;
@@ -10,10 +12,13 @@ import br.edu.ifrn.laj.pdcorp.apisea.models.User;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
+	@Transactional
 	Subscription findByUserAndEvent(User user, Event event);
 	
+	@Transactional
 	List<Subscription> findAllByEvent(Event event);
 	
+	@Transactional
 	List<Subscription> findAllByUser(User user);
 	
 }
