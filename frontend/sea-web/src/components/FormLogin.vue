2be4e53c9c17d38c0f01d7b1a209d005728b734e
Fernando Soares <fernandolinharessoares@gmail.com>
2020-11-05 18:16:55 -0300

    Criando objeto de login e vinculando ao formulário

--- frontend/sea-web/src/components/FormLogin.vue
+++ frontend/sea-web/src/components/FormLogin.vue
@@ -1,47 +1,78 @@
 <template>
   <div class="formulario">
-		<form>
-			<h1>Entrar</h1>
-			<hr>
-			<label for="email">Email</label>
-			<input type="text" 
-			class="form-control form-control-lg" 
-			name="email" 
-			id="email" 
-			placeholder="joao@exemplo.com"
-			required>
+    <form @submit.prevent="login">
+      <h1>Entrar</h1>
+      <hr />
+      <label for="email">Email</label>
+      <input
+        v-model="credentials.user"
+        type="email"
+        class="form-control form-control-lg"
+        name="email"
+        id="email"
+        placeholder="joao@exemplo.com"
+        required
+      />
 
-			<label for="senha">Senha</label>
-			<input type="password" 
-			class="form-control form-control-lg" 
-			name="senha" 
-			id="senhalogin" 
-			placeholder="••••••"
-			required>
+      <label for="senha">Senha</label>
+      <input
+        v-model="credentials.password"
+        type="password"
+        class="form-control form-control-lg"
+        name="senha"
+        id="senhalogin"
+        placeholder="••••••"
+        required
+      />
 
-             <button class=" button btn"> <router-link to="/events">Entrar </router-link> </button> 
-		</form>
-	</div>
+      <button type="submit" class="button btn">Entrar</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import LoginService from "../services/login";
 export default {
-    name: 'FormLogin'
-
-}
-
+  name: "FormLogin",
+  data() {
+    return {
+      credentials: {
+        user: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      console.log("Chamou o login");
+      console.log(this.credentials);
+      LoginService.login(this.credentials)
+        .then(response => {
+          console.log("Deu certo!");
+          console.log(response);
+        })
+        .catch(e => {
+          console.log("Deu errado!");
+          console.log(e);
+        });
+      this.clearForm();
+    },
+    clearForm() {
+      this.credentials = {};
+    }
+  }
+};
 </script>
 
 <style scoped>
-
-.formulario{
-	width: 30%;
-	margin-left:5%;
+.formulario {
+  width: 30%;
+  margin-left: 5%;
 }
 
-form button{
-	width: 100%;
-	margin-top: 5%;
-	margin-left: 0px;
+form button {
+  width: 100%;
+  margin-top: 5%;
+  margin-left: 0px;
 }
 </style>
