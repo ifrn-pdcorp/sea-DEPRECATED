711b4be980fc4a19e7bb9801e86b378db485c4a5
Fernando Soares <fernandolinharessoares@gmail.com>
2020-11-10 20:39:51 -0300

    Agora sim com a solução correta para o problema do CORS

--- backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/security/WebSecurityConfig.java
+++ backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package br.edu.ifrn.laj.pdcorp.apisea.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,8 +8,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import br.edu.ifrn.laj.pdcorp.apisea.filters.CORSFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -32,8 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.addFilterBefore(new JwtLoginFilter("/login", authenticationManager()),
 						UsernamePasswordAuthenticationFilter.class)
 
-				.addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-				.addFilterBefore(new CORSFilter(), JwtAuthenticationFilter.class);
+				.addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.cors();
 	}
 
 	@Override
