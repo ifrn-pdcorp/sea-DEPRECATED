cf20901eb1eb4a8dcd28f8f59b9999e73581bd7e
Fernando Soares <fernandolinharessoares@gmail.com>
2020-11-23 20:02:27 -0300

    Criando endpoint para retornar os dados do usuário e o token da sessão que está logada

--- backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/controllers/UserController.java
+++ backend/api-sea/src/main/java/br/edu/ifrn/laj/pdcorp/apisea/controllers/UserController.java
@@ -1,8 +1,12 @@
 package br.edu.ifrn.laj.pdcorp.apisea.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifrn.laj.pdcorp.apisea.dtos.SessionDTO;
 import br.edu.ifrn.laj.pdcorp.apisea.dtos.UserDTO;
 import br.edu.ifrn.laj.pdcorp.apisea.exceptions.ApiException;
 import br.edu.ifrn.laj.pdcorp.apisea.models.User;
@@ -30,6 +35,20 @@ public class UserController {
 	public ResponseEntity<?> saveOrEditUser(@RequestBody User user) {
 		try {
 			return new ResponseEntity<UserDTO>(this.userService.save(user), HttpStatus.OK);
+		} catch (ApiException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@ApiOperation(value = "Carrega os dados do usuário que está logado na sessão")
+	@GetMapping("/loadsession")
+	public ResponseEntity<?> loadSession(Authentication principal) {
+		try {
+			SessionDTO sessionDTO = new SessionDTO();
+			sessionDTO.setUser(this.userService.findByUsername(principal.getName()));
+			sessionDTO.setToken(principal.getCredentials().toString());
+
+			return new ResponseEntity<SessionDTO>(sessionDTO, HttpStatus.OK);
 		} catch (ApiException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
