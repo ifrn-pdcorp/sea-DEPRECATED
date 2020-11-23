367370ba925c6f6b61c8c765dac9f928c2b97f62
Fernando Soares <fernandolinharessoares@gmail.com>
2020-11-23 17:57:56 -0300

    Configurando interceptor para adicionar cabeçalho quando houver token salvo no localStorage

--- frontend/sea-web/src/services/config.js
+++ frontend/sea-web/src/services/config.js
@@ -3,3 +3,10 @@ import axios from 'axios'
 export const http = axios.create({
     baseURL: 'http://localhost:8080/'
 })
+
+http.interceptors.request.use(config => {
+    var token = localStorage.getItem('userToken')
+    if (token)
+        config.headers.common['Authorization'] = token
+    return config
+})
